063f43ea74a1688252a8aa321f62a8335a44c5a5
sigitp-git <sigitp@amazon.com>
2020-06-11 22:16:43 -0500

    redux

--- redux/redux-expense-manager.js
+++ redux/redux-expense-manager.js
@@ -105,6 +105,19 @@ const filtersReducer = (state = filtersReducerDefState, action) => {
   }
 }
 
+// Get Filtered Expenses
+// timestamp in milliseconds, timezone independent, unix epoch, jan 01 1970
+const getFilteredExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+    return expenses.filter((exp) => {
+        // returns true for match, match means shown on the filteredNotes=getFilteredNotes()
+        const textMatch = exp.description.toLowerCase().match(text) || exp.note.toLowerCase().match(text)
+        const startDateMatch = typeof startDate !== 'number' || exp.createdAt >= startDate
+        const endDateMatch = typeof endDate !== 'number' || exp.createdAt <= endDate
+
+        return textMatch && startDateMatch && endDateMatch
+    })
+}
+
 // @@@@@@@@@@@Create the State/Store (written as 3rd part)
 const store = createStore(
   combineReducers({
@@ -114,21 +127,21 @@ const store = createStore(
 )
 
 store.subscribe(() => {
-  console.log(store.getState())
+  const state = store.getState()
+  const filteredExpenses = getFilteredExpenses(state.expenses, state.filters)
+  console.log(filteredExpenses)
 })
 
 // // @@@@@@@@@@@DISPATCHES
 // // Writing how function being called first then work out the function details is good practice
 // // Expenses State Dispatch
-// const expense1 = store.dispatch(
-//   addExpense({ description: 'Rent', amount: 100 })
-// )
-// const expense2 = store.dispatch(addExpense({ description: 'Car', amount: 300 }))
+const expense1 = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 500 }))
+const expense2 = store.dispatch(addExpense({ description: 'Car', amount: 300, createdAt: 1500 }))
 // store.dispatch(removeExpense({ id: expense1.expense.id }))
 // store.dispatch(editExpense(expense2.expense.id, { amount: 800 }))
 
 // // Filter State Dispatch
-// store.dispatch(setTextFilter('rent'))
+store.dispatch(setTextFilter())
 // store.dispatch(sortByAmount())
 // store.dispatch(sortByDate())
 store.dispatch(setStartDate(125))
